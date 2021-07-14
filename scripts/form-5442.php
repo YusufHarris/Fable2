0f8ededdfe3d91133c65cb5d93612a61ca1962b7
YusufHarris <kurolionatus@gmail.com>
2021-07-14 09:27:35 -0500

    Nicepage Import

---
+++ scripts/form-5442.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once('FormProcessor.php');
+
+$form = array(
+    'subject' => 'Feedback',
+    'email_message' => '',
+    'success_redirect' => '',
+    'sendIpAddress' => true,
+    'email' => array(
+    'from' => 'feedback@fableforgrowth.com',
+    'to' => 'shibacommunity@gmail.com'
+    ),
+    'fields' => array(
+    'name' => array(
+    'order' => 1,
+    'type' => 'string',
+    'label' => 'name',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'name\' is required.'
+    )
+    ),
+    'email' => array(
+    'order' => 2,
+    'type' => 'email',
+    'label' => 'email',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'email\' is required.'
+    )
+    ),
+    'message' => array(
+    'order' => 3,
+    'type' => 'string',
+    'label' => 'message',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'message\' is required.'
+    )
+    ),
+    )
+    );
+
+    $processor = new FormProcessor('');
+    $processor->process($form);
+
+    ?>
